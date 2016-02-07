0f91d06c4ab82bbcc30ed374734719c6ecad5ff2
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2016-02-07 12:14:00 +0100

    working less->css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+// require jquery_ujs
 //= require jquery.ba-throttle-debounce
 //= require bootstrap
 //= require masonry
